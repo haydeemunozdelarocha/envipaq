e9a8821df72252ad77a1364f31a6d4012f4d356c
Haydeé Muñoz <haydeemunozdelarocha@gmail.com>
2017-03-19 16:51:20 -0600

    changed font

--- db.js
+++ db.js
@@ -3,17 +3,19 @@ var mysql = require('promise-mysql');
 
 var db_config      =    mysql.createPool({
     connectionLimit : 150, //important
-    host     : process.env.DB_HOST,
-    user     : process.env.DB_USER,
-    password : process.env.DB_PSS,
+    host     : process.env.MASPOST_DB_HOST,
+    user     : process.env.MASPOST_DB_USER,
+    password : process.env.MASPOST_DB_PSS,
     database : 'heroku_b6e69546b135d2b',
     debug    :  false,
     multipleStatements: true,
+    queueLimit: 50,
     acquireTimeout:1000000,
     connectTimeout:0
 });
+console.log(process.env.DB_HOST)
 var conn;
-function handle_database(req,res) {
+function handle_database() {
   console.log('connecting')
 db_config.getConnection().then(function(connection) {
     conn = connection;
@@ -22,46 +24,46 @@ db_config.getConnection().then(function(connection) {
     console.log("Connect failed", error);
    setTimeout(handleDisconnect, 2000);
    return
-  }).finally(function() {
-    if (conn) {
-      console.log('done using connection')
-      return
-    }
-  });
-  console.log('Conectado');
+  })
 }
 
+// db_config.on('error', function(err) {
+//     console.log('db error', err);
+//     if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+//       handleDisconnect();
+//       console.log('reconnecting')
+//     } else if(err.code === 'ECONNREFUSED'){
+//       console.log(err.code)
+//       handleDisconnect();
+//     } else if(err.code === 'ER_BAD_FIELD_ERROR'){
+//       console.log(err.code)
+//       db_config.end();
+//       handleDisconnect();
+//     } else if(err.code === 'ER_USER_LIMIT_REACHED'){
+//       console.log(err.code)
+//       handleDisconnect();
+//     }else {
+//       handleDisconnect();
+//     }
+//   });
 
-db_config.on('error', function(err) {
-    console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-      handleDisconnect();
-      console.log('reconnecting')                       // lost due to either server restart, or a
-    } else if(err.code === 'ECONNREFUSED'){
-      console.log(err.code)
-      handleDisconnect();
-    } else if (err.code === 'ER_USER_LIMIT_REACHED'){
-      console.log(err.code)
-      handleDisconnect();
-    }else {                           // connnection idle timeout (the wait_timeout
-      handleDisconnect();                                // server variable configures this)
-    }
-  });
 
-function handleDisconnect() {
+function handleDisconnect(res,req,err) {
 console.log('reconnecting...')
-db_config.getConnection(function(err){
-  if(err){
-    console.log('No se pudo conectar a la base de datos');
-    handle_database();
-    return;
-  }
-  console.log('Conectado');
-});
+handle_database()
 }
 
 handle_database();
 
+function disconnect() {
+  db_config.end(function onEnd(error) {
+  console.log('ending connection')
+  db.end()
+  if (error) throw error;
+  // All connections are now closed once they have been returned with connection.release()
+  // i.e. this waits for all consumers to finish their use of the connections and ends them.
+});
+}
 
 
 
